a530fa8f822a1eec293bb6f1e729a34cd911fd23
dariel0514 <dariel0514@hotmail.com>
2022-09-04 15:46:25 +0000

    Done

--- src/app.js
+++ src/app.js
@@ -6,6 +6,15 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let a = 0; a < pronoun.length; a++) {
+      for (let b = 0; b < pronoun.length; b++) {
+        console.log(`${pronoun[i]}${adj[a]}${noun[b]}.com`);
+      }
+    }
+  }
 };
